2f8c2f1f0d5ea65948fe18a4711811f9f8ed641c
genegwells21 <46447933+genegwells21@users.noreply.github.com>
2020-01-01 00:37:06 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+var c = document.createElement("canvas");
+var ctx = c.getContext("2d");
+c.width = 500;
+c.height= 350;
+document.body.appendChild(c);
+
+var perm = [];
+while (perm.length < 255) {
+while(perm.includes(val = Math.floor(Math.random() * 255)));
